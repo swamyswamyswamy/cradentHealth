7aec6f0bd01cddb214ff3cfba12453fd16b6f870
swamyswamy <swamyswamy>
2025-02-10 22:58:40 +0530

    profile screen

--- lib/view/screens/drawer_screens/drawer_screen.dart
+++ lib/view/screens/drawer_screens/drawer_screen.dart
@@ -5,6 +5,7 @@ import 'package:cradenthealth/constants/app_images.dart';
 import 'package:cradenthealth/constants/app_mediaquery.dart';
 import 'package:cradenthealth/constants/app_sizedbox.dart';
 import 'package:cradenthealth/constants/app_text.dart';
+import 'package:cradenthealth/view/screens/drawer_screens/profile_screen.dart';
 import 'package:cradenthealth/view_model/ui_controllers/bookings_controller.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -23,145 +24,171 @@ class DrawerScreen extends StatelessWidget {
           surfaceTintColor: AppColors.whiteColor,
         ),
         backgroundColor: AppColors.whiteColor,
-        body: Column(
-          children: [
-            Stack(
-              children: [
-                AppButton(
-                  height: 70,
-                  width: 70,
-                  borderColor: AppColors.blackColor.withOpacity(0.1),
-                  backgroundColor: AppColors.whiteColor,
-                  borderRadius: 18,
-                  child: Padding(
-                      padding: EdgeInsets.all(getProportionateScreenHeight(4)),
-                      child: Container(
-                        decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            image: DecorationImage(
-                                image: NetworkImage(
-                                    "https://www.shutterstock.com/image-photo/head-shot-portrait-close-smiling-600nw-1714666150.jpg"))),
-                      )),
-                ),
-                Positioned(
-                  bottom: -5,
-                  right: -15,
-                  child: Container(
-                    height: getProportionateScreenHeight(30),
-                    width: getProportionateScreenWidth(30),
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle, color: AppColors.whiteColor),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  AppButton(
+                    height: 70,
+                    width: 70,
+                    borderColor: AppColors.blackColor.withOpacity(0.1),
+                    backgroundColor: AppColors.whiteColor,
+                    borderRadius: 18,
                     child: Padding(
-                      padding: EdgeInsets.all(getProportionateScreenHeight(2)),
-                      child: Container(
-                        height: getProportionateScreenHeight(30),
-                        width: getProportionateScreenWidth(30),
-                        decoration: BoxDecoration(
-                            shape: BoxShape.circle, color: AppColors.blueColor),
-                        child: Center(
-                            child: Icon(
-                          Icons.edit,
-                          color: AppColors.whiteColor,
-                          size: 15,
+                        padding:
+                            EdgeInsets.all(getProportionateScreenHeight(4)),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              image: DecorationImage(
+                                  image: NetworkImage(
+                                      "https://www.shutterstock.com/image-photo/head-shot-portrait-close-smiling-600nw-1714666150.jpg"))),
                         )),
+                  ),
+                  Positioned(
+                    bottom: -5,
+                    right: -15,
+                    child: Container(
+                      height: getProportionateScreenHeight(30),
+                      width: getProportionateScreenWidth(30),
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle, color: AppColors.whiteColor),
+                      child: Padding(
+                        padding:
+                            EdgeInsets.all(getProportionateScreenHeight(2)),
+                        child: Container(
+                          height: getProportionateScreenHeight(30),
+                          width: getProportionateScreenWidth(30),
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: AppColors.blueColor),
+                          child: Center(
+                              child: Icon(
+                            Icons.edit,
+                            color: AppColors.whiteColor,
+                            size: 15,
+                          )),
+                        ),
                       ),
                     ),
-                  ),
-                )
-              ],
-              alignment: Alignment.bottomRight,
-              clipBehavior: Clip.none,
-            ),
-            CustomSizedBoxHeight(height: 8),
-            CustomText(
-                textName: "Narasimha varma",
-                textColor: AppColors.blackColor,
-                fontWeightType: FontWeightType.medium,
-                fontFamily: FontFamily.inter,
-                fontSize: 16),
-            CustomSizedBoxHeight(height: 6),
-            CustomText(
-                textName: "Narasimhavarma123@gmail.com",
-                textColor: AppColors.blackColor.withOpacity(0.6),
-                fontWeightType: FontWeightType.regular,
-                fontFamily: FontFamily.inter,
-                fontSize: 12),
-            CustomSizedBoxHeight(height: 6),
-            CustomText(
-                textName: "9898989898",
-                textColor: AppColors.blackColor.withOpacity(0.6),
-                fontWeightType: FontWeightType.regular,
-                fontFamily: FontFamily.inter,
-                fontSize: 12),
-            CustomSizedBoxHeight(height: 9),
-            Container(
-              color: AppColors.secondaryColor,
-              height: getProportionateScreenHeight(3),
-            ),
-            CustomSizedBoxHeight(height: 34),
-            Padding(
-              padding: EdgeInsets.symmetric(
-                  horizontal: getProportionateScreenWidth(16)),
-              child: ListView.builder(
-                itemCount: 5,
-                shrinkWrap: true,
-                itemBuilder: (BuildContext context, int index) {
-                  return Padding(
-                    padding: EdgeInsets.only(
-                        bottom: getProportionateScreenWidth(28)),
-                    child: Row(
-                      children: [
-                        AppButton(
-                          height: 42,
-                          width: 42,
-                          backgroundColor: AppColors.secondaryColor,
-                          borderRadius: 3,
-                          child: Center(
-                            child: Image.asset(
-                              AppImages.forgotImage,
-                              height: getProportionateScreenHeight(30),
-                              width: getProportionateScreenWidth(30),
+                  )
+                ],
+                alignment: Alignment.bottomRight,
+                clipBehavior: Clip.none,
+              ),
+              CustomSizedBoxHeight(height: 8),
+              CustomText(
+                  textName: "Narasimha varma",
+                  textColor: AppColors.blackColor,
+                  fontWeightType: FontWeightType.medium,
+                  fontFamily: FontFamily.inter,
+                  fontSize: 16),
+              CustomSizedBoxHeight(height: 6),
+              CustomText(
+                  textName: "Narasimhavarma123@gmail.com",
+                  textColor: AppColors.blackColor.withOpacity(0.6),
+                  fontWeightType: FontWeightType.regular,
+                  fontFamily: FontFamily.inter,
+                  fontSize: 12),
+              CustomSizedBoxHeight(height: 6),
+              CustomText(
+                  textName: "9898989898",
+                  textColor: AppColors.blackColor.withOpacity(0.6),
+                  fontWeightType: FontWeightType.regular,
+                  fontFamily: FontFamily.inter,
+                  fontSize: 12),
+              CustomSizedBoxHeight(height: 9),
+              Container(
+                color: AppColors.secondaryColor,
+                height: getProportionateScreenHeight(3),
+              ),
+              CustomSizedBoxHeight(height: 34),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: getProportionateScreenWidth(16)),
+                child: ListView.builder(
+                  itemCount: 5,
+                  shrinkWrap: true,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Padding(
+                      padding: EdgeInsets.only(
+                          bottom: getProportionateScreenWidth(28)),
+                      child: InkWell(
+                        onTap: () {
+                          Get.to(ProfileScreen());
+                        },
+                        child: Row(
+                          children: [
+                            AppButton(
+                              height: 42,
+                              width: 42,
+                              backgroundColor: AppColors.secondaryColor,
+                              borderRadius: 3,
+                              child: Center(
+                                child: Image.asset(
+                                  AppImages.forgotImage,
+                                  height: getProportionateScreenHeight(30),
+                                  width: getProportionateScreenWidth(30),
+                                ),
+                              ),
                             ),
-                          ),
-                        ),
-                        CustomSizedBoxWidth(width: 16),
-                        Expanded(
-                          child: Column(
-                            children: [
-                              Row(
+                            CustomSizedBoxWidth(width: 16),
+                            Expanded(
+                              child: Column(
                                 children: [
-                                  CustomText(
-                                      textName: index == 0
-                                          ? "Wallet"
-                                          : index == 1
-                                              ? "Bookings"
-                                              : index == 2
-                                                  ? "Prescription"
-                                                  : index == 3
-                                                      ? "Family Members"
-                                                      : "Settings",
-                                      textColor: AppColors.blackColor,
-                                      fontWeightType: FontWeightType.medium,
-                                      fontFamily: FontFamily.inter,
-                                      fontSize: 14),
-                                  Spacer(),
-                                  Icon(Icons.navigate_next)
+                                  Row(
+                                    children: [
+                                      CustomText(
+                                          textName: index == 0
+                                              ? "Wallet"
+                                              : index == 1
+                                                  ? "Bookings"
+                                                  : index == 2
+                                                      ? "Prescription"
+                                                      : index == 3
+                                                          ? "Family Members"
+                                                          : "Settings",
+                                          textColor: AppColors.blackColor,
+                                          fontWeightType: FontWeightType.medium,
+                                          fontFamily: FontFamily.inter,
+                                          fontSize: 14),
+                                      Spacer(),
+                                      Icon(Icons.navigate_next)
+                                    ],
+                                  ),
+                                  CustomSizedBoxHeight(height: 13),
+                                  Divider(
+                                    color:
+                                        AppColors.blackColor.withOpacity(0.2),
+                                  )
                                 ],
                               ),
-                              CustomSizedBoxHeight(height: 13),
-                              Divider(
-                                color: AppColors.blackColor.withOpacity(0.2),
-                              )
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                  );
-                },
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
               ),
-            ),
-          ],
+              CustomSizedBoxHeight(height: 15),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: getProportionateScreenHeight(16)),
+                child: AppButton(
+                  height: 44,
+                  backgroundColor: AppColors.redColor,
+                  onTap: () {
+                    // Get.to(HomeScreen());
+                  },
+                  hasShadow: true,
+                  label: "Logout",
+                ),
+              ),
+              CustomSizedBoxHeight(height: 15),
+            ],
+          ),
         ));
   }
 }
---
+++ lib/view/screens/drawer_screens/profile_screen.dart
@@ -0,0 +1,155 @@
+import 'package:cradenthealth/constants/app_button.dart';
+import 'package:cradenthealth/constants/app_colors.dart';
+import 'package:cradenthealth/constants/app_dropdown.dart';
+import 'package:cradenthealth/constants/app_images.dart';
+import 'package:cradenthealth/constants/app_mediaquery.dart';
+import 'package:cradenthealth/constants/app_sizedbox.dart';
+import 'package:cradenthealth/constants/app_text.dart';
+import 'package:cradenthealth/constants/app_textfields.dart';
+import 'package:cradenthealth/view_model/ui_controllers/bookings_controller.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
+
+class ProfileScreen extends StatelessWidget {
+  ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: AppColors.whiteColor,
+          surfaceTintColor: AppColors.whiteColor,
+        ),
+        backgroundColor: AppColors.whiteColor,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  AppButton(
+                    height: 100,
+                    width: 100,
+                    borderColor: AppColors.blackColor.withOpacity(0.1),
+                    backgroundColor: AppColors.whiteColor,
+                    borderRadius: 18,
+                    child: Padding(
+                        padding:
+                            EdgeInsets.all(getProportionateScreenHeight(4)),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              image: DecorationImage(
+                                  image: NetworkImage(
+                                      "https://www.shutterstock.com/image-photo/head-shot-portrait-close-smiling-600nw-1714666150.jpg"))),
+                        )),
+                  ),
+                  Positioned(
+                    bottom: -5,
+                    right: -15,
+                    child: Container(
+                      height: getProportionateScreenHeight(40),
+                      width: getProportionateScreenWidth(40),
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle, color: AppColors.whiteColor),
+                      child: Padding(
+                        padding:
+                            EdgeInsets.all(getProportionateScreenHeight(2)),
+                        child: Container(
+                          height: getProportionateScreenHeight(30),
+                          width: getProportionateScreenWidth(30),
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: AppColors.blueColor),
+                          child: Center(
+                              child: Icon(
+                            Icons.edit,
+                            color: AppColors.whiteColor,
+                            size: 15,
+                          )),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+                alignment: Alignment.bottomRight,
+                clipBehavior: Clip.none,
+              ),
+              CustomSizedBoxHeight(height: 27),
+              Container(
+                color: AppColors.secondaryColor,
+                height: getProportionateScreenHeight(9),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: getProportionateScreenWidth(16)),
+                child: Column(
+                  children: [
+                    CustomSizedBoxHeight(height: 28),
+                    Inputfield(
+                        fillColor: AppColors.secondaryColor,
+                        controller: TextEditingController(),
+                        // maxLength: 10,
+                        label: "Name",
+                        keyboardType: TextInputType.text,
+                        hinttext: ""),
+                    CustomSizedBoxHeight(height: 12),
+                    Inputfield(
+                        fillColor: AppColors.secondaryColor,
+                        controller: TextEditingController(),
+                        // maxLength: 10,
+                        label: "Mobile Number",
+                        keyboardType: TextInputType.text,
+                        hinttext: ""),
+                    CustomSizedBoxHeight(height: 12),
+                    Inputfield(
+                        fillColor: AppColors.secondaryColor,
+                        controller: TextEditingController(),
+                        // maxLength: 10,
+                        label: "Email",
+                        keyboardType: TextInputType.text,
+                        hinttext: ""),
+                    CustomSizedBoxHeight(height: 12),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Inputfield(
+                              fillColor: AppColors.secondaryColor,
+                              controller: TextEditingController(),
+                              // maxLength: 10,
+                              label: "Age",
+                              keyboardType: TextInputType.text,
+                              hinttext: ""),
+                        ),
+                        CustomSizedBoxWidth(width: 40),
+                        Expanded(
+                          child: Inputfield(
+                              fillColor: AppColors.secondaryColor,
+                              controller: TextEditingController(),
+                              // maxLength: 10,
+                              label: "Gender",
+                              keyboardType: TextInputType.text,
+                              hinttext: ""),
+                        ),
+                      ],
+                    ),
+                    CustomSizedBoxHeight(height: 58),
+                    CustomSizedBoxHeight(height: 26),
+                    AppButton(
+                      height: 44,
+                      onTap: () {
+                        // Get.to(OtpVerificationScreen());
+                      },
+                      hasShadow: true,
+                      label: "Save",
+                    ),
+                  ],
+                ),
+              ),
+              CustomSizedBoxHeight(height: 30),
+            ],
+          ),
+        ));
+  }
+}
